7740ec5cbefaa0325b373f90812e3fb73d4a395f
Sany-SHiriCE <Sany-SHiriCE@316D-3>
2023-12-05 12:07:23 +0700

    Добавьте файлы проекта.

---
+++ ClassStackNew/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassStack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack stack1 = new Stack(5);
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Print();
+            stack1.Peek();
+            Console.WriteLine($"Среднее значение: {stack1.GetAverage()}");
+
+            int removedValue = stack1.Pop();
+            Console.WriteLine($"Удаленный элемент: {removedValue}");
+
+            Stack stack2 = new Stack(stack1);
+            stack2.Pop();
+            stack2.Print();
+            stack2.Peek();
+            Console.WriteLine($"Среднее значение: {stack2.GetAverage()}");
+        }
+    }
+}
---
+++ ClassStackNew/Stack.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ClassStack
+{
+    class Stack
+    {
+        private class Node
+        {
+            public int Value;
+            public Node Next;
+
+            public Node(int value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private Node top;
+        private int size;
+        private int maxSize;
+        private int sum;
+
+        public Stack(int maxSize)
+        {
+            this.maxSize = maxSize;
+            size = 0;
+            top = null;
+            sum = 0;
+        }
+
+        public Stack(Stack otherStack)
+        {
+            maxSize = otherStack.maxSize;
+            size = 0;
+            top = null;
+            sum = 0;
+
+            Node current = otherStack.top;
+            while (current != null)
+            {
+                Push(current.Value);
+                current = current.Next;
+            }
+        }
+
+        public bool IsFull()
+        {
+            return size >= maxSize;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public void Push(int value)
+        {
+            if (!IsFull())
+            {
+                Node newNode = new Node(value);
+                newNode.Next = top;
+                top = newNode;
+                size++;
+                sum += value;
+                Console.WriteLine($"Добавлен: {value}");
+            }
+            else
+            {
+                Console.WriteLine("Стек переполнен. Невозможно добавить элемент.");
+            }
+        }
+
+        public int Pop()
+        {
+            if (!IsEmpty())
+            {
+                int removedValue = top.Value;
+                top = top.Next;
+                size--;
+                sum -= removedValue;
+                Console.WriteLine($"Удален: {removedValue}");
+                return removedValue;
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст. Невозможно удалить элемент.");
+                return -1; // Возвращаем специальное значение для обозначения ошибки
+            }
+        }
+
+        public void Peek()
+        {
+            if (!IsEmpty())
+            {
+                int topValue = top.Value;
+                Console.WriteLine($"Верхний элемент стека: {topValue}");
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст. Нет элементов для просмотра.");
+            }
+        }
+
+        public void Print()
+        {
+            if (!IsEmpty())
+            {
+                Console.WriteLine("Элементы стека:");
+                Node current = top;
+                while (current != null)
+                {
+                    Console.WriteLine(current.Value);
+                    current = current.Next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст. Нет элементов для вывода.");
+            }
+        }
+
+        public double GetAverage()
+        {
+            if (!IsEmpty())
+            {
+                return (double)sum / size;
+            }
+            else
+            {
+                Console.WriteLine("Стек пуст. Нет элементов для вычисления среднего значения.");
+                return 0;
+            }
+        }
+    }
+}
